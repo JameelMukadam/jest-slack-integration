ed9ee179ae8b9a59034a5fc3981051968e7ec9da
Jameel Mukadam <jameel@jameelmukadam.com>
2018-07-04 11:29:47 +0200

    Feature: updated the slack message to display more details

--- index.js
+++ index.js
@@ -6,25 +6,71 @@ const bugsnag = require('bugsnag')
  * function to send slack your test notifications
  * @param {*} testResults this is the parameter passed through by Jest containing the test results
  */
-module.exports = async testResults => {
+module.exports = testResults => {
   try {
     const packageJson = readPkg.sync(process.cwd())
 
     bugsnag.register(packageJson.jestSlackIntegration.bugsnagAPI)
 
     const failedMessage = `<!here> Bad News! *${testResults.numFailedTestSuites}* test suites have failed and *${testResults.numFailedTests}* tests have failed :( *${testResults.numPassedTests}* tests have passed. Please have a look at your tests.`
-    const passedMessage = `<!here> Great News! All tests have passed! Code coverage is coming soon..`
+    const passedMessage = `<!here> Great News! All *${testResults.numPassedTests}* tests have passed! Code coverage is coming soon..`
     const text = (testResults.numFailedTestSuites > 0 || testResults.numFailedTests > 0 ) ? failedMessage : passedMessage
+
+    const slackMessage = {
+      text: text,
+      attachments: [
+        {
+          fallback: 'Required plain-text summary of the attachment.',
+          color: '#36a64f',
+          title: 'Your Test Results Are In!',
+          mrkdwn_in: ['text'],
+          text: 'Here is a quick summary of the jest test results..   \nrun `jest` locally to see all the details \n..',
+          fields: [
+              {
+                title: 'Total Tests Suites',
+                value: testResults.numTotalTestSuites,
+                short: false
+              },
+              {
+                title: 'Total Tests',
+                value: testResults.numTotalTests,
+                short: false
+              },
+              {
+                  title: 'Test Suites Passing',
+                  value: testResults.numPassedTestSuites,
+                  short: false
+              },
+              {
+                  title: 'Test Suites Failing',
+                  value: testResults.numFailedTestSuites,
+                  short: false
+              },
+              {
+                  title: 'Total Tests Passing',
+                  value: testResults.numPassedTests,
+                  short: false
+              },
+              {
+                  title: 'Total Tests Failing',
+                  value: testResults.numFailedTests,
+                  short: false
+              }
+          ],
+          'footer': 'jest-slack-integration by Jameel Mukadam'
+        }
+      ]
+    }
 
     const options = {
       uri: packageJson.jestSlackIntegration.webhookUrl,
       method: 'POST',
-      json: { text },
+      json: slackMessage,
       mrkdwn: true
     }
 
     try {
-      await request(options)
+      request(options)
     } catch (err) {
       bugsnag.notify(err)
     }
